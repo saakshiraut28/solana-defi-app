e406fe2c898464502040132b4f38d01f53a5936b
Saakshi Raut <saakshiraut28@gmail.com>
2024-08-25 00:27:25 +0530

    add: search tab

--- frontend/src/components/allCoin.tsx
+++ frontend/src/components/allCoin.tsx
@@ -2,6 +2,7 @@
 
 import { useContext, useEffect, useState } from "react";
 import { CoinContext } from "../context/coinContext";
+import { Link } from "react-router-dom";
 
 function AllCoins() {
   const coinContext = useContext(CoinContext);
@@ -10,6 +11,22 @@ function AllCoins() {
   }
   const { allCoin, currency } = coinContext;
   const [displayCoin, setDisplayCoin] = useState<any[]>([]);
+  const [input, setInput] = useState("");
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    if (e.target.value === "") {
+      setDisplayCoin(allCoin);
+    }
+  };
+
+  const searchHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const coins = allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase());
+    });
+    setDisplayCoin(coins);
+  };
 
   useEffect(() => {
     setDisplayCoin(allCoin);
@@ -18,6 +35,17 @@ function AllCoins() {
   return (
     <div>
       <h3>All Coins Data</h3>
+      <form onSubmit={searchHandler}>
+        <input
+          required
+          value={input}
+          onChange={inputHandler}
+          type="text"
+          placeholder="Search Coin or Token"
+        />
+        <button type="submit">Search</button>
+      </form>
+
       <div className="mx-2 lg:mx-8 my-8 px-4 py-4">
         <table className="border-gray-300 border min-w-full">
           <thead className="border-gray-300 bg-gray-100 border-b text-black">
@@ -27,10 +55,11 @@ function AllCoins() {
               <th className="px-4 py-2 border-r">Price</th>
               <th className="px-4 py-2 border-r">24H Change</th>
               <th className="px-4 py-2">Market Cap</th>
+              <th className="px-4 py-2">View Chart</th>
             </tr>
           </thead>
           <tbody>
-            {displayCoin.slice(0, 10).map((item) => (
+            {displayCoin.slice(0, 10).map((item, index) => (
               <tr key={item.id} className="border-gray-300 border-b">
                 <td className="px-4 py-2">{item.market_cap_rank}</td>
                 <td className="flex items-center gap-3 px-4 py-2">
@@ -55,6 +84,9 @@ function AllCoins() {
                 <td className="px-4 py-2">
                   {currency.symbol}
                   {item.market_cap.toLocaleString()}
+                </td>
+                <td className="px-4 py-2">
+                  <Link to={`/coin/${item.id}`}>View Chart</Link>
                 </td>
               </tr>
             ))}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/** @format */
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Router } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
